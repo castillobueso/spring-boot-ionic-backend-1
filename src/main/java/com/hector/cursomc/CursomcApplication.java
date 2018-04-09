17451227617782ef9fbf6f7e07a9955c10519e3b
Hector Castillo <castillobueso@yahoo.com>
2018-04-08 23:13:47 -0300

    Cliente. TipoCliente. telefones e enderecos

--- src/main/java/com/hector/cursomc/CursomcApplication.java
+++ src/main/java/com/hector/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.hector.cursomc;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,10 +10,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.hector.cursomc.domain.Categoria;
 import com.hector.cursomc.domain.Cidade;
+import com.hector.cursomc.domain.Cliente;
+import com.hector.cursomc.domain.Endereco;
 import com.hector.cursomc.domain.Estado;
 import com.hector.cursomc.domain.Produto;
+import com.hector.cursomc.domain.enums.TipoCliente;
 import com.hector.cursomc.repositories.CategoriaRepository;
 import com.hector.cursomc.repositories.CidadeRepository;
+import com.hector.cursomc.repositories.ClienteRepository;
+import com.hector.cursomc.repositories.EnderecoRepository;
 import com.hector.cursomc.repositories.EstadoRepository;
 import com.hector.cursomc.repositories.ProdutoRepository;
 
@@ -27,7 +33,11 @@ public class CursomcApplication implements CommandLineRunner {
 	private EstadoRepository estadoRepository;
 	@Autowired
 	private CidadeRepository cidadeRepository;
-
+	@Autowired	
+	private ClienteRepository clienteRepository;
+	@Autowired	
+	private EnderecoRepository enderecoRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
@@ -64,6 +74,17 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		estadoRepository.save(Arrays.asList(est1, est2));
 		cidadeRepository.save(Arrays.asList(c1,c2,c3));
-				
+		
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		
+		cli1.getTelefones().addAll(Arrays.asList("27363323","93838393"));
+		
+		Endereco e1 =  new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
+		
+		clienteRepository.save(Arrays.asList(cli1));
+		enderecoRepository.save(Arrays.asList(e1,e2));
 	}
 }
--- src/main/java/com/hector/cursomc/domain/Categoria.java
+++ src/main/java/com/hector/cursomc/domain/Categoria.java
@@ -3,13 +3,11 @@ package com.hector.cursomc.domain;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
-
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
