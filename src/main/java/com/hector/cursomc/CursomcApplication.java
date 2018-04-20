d0a06436d02e3bfe5f1e70038cb517abfe6f9a44
Hector Castillo <castillobueso@yahoo.com>
2018-04-20 15:35:35 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/hector/cursomc/CursomcApplication.java
+++ src/main/java/com/hector/cursomc/CursomcApplication.java
@@ -1,7 +1,6 @@
 package com.hector.cursomc;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +13,7 @@ import com.hector.cursomc.domain.Cidade;
 import com.hector.cursomc.domain.Cliente;
 import com.hector.cursomc.domain.Endereco;
 import com.hector.cursomc.domain.Estado;
+import com.hector.cursomc.domain.ItemPedido;
 import com.hector.cursomc.domain.Pagamento;
 import com.hector.cursomc.domain.PagamentoComBoleto;
 import com.hector.cursomc.domain.PagamentoComCartao;
@@ -26,6 +26,7 @@ import com.hector.cursomc.repositories.CidadeRepository;
 import com.hector.cursomc.repositories.ClienteRepository;
 import com.hector.cursomc.repositories.EnderecoRepository;
 import com.hector.cursomc.repositories.EstadoRepository;
+import com.hector.cursomc.repositories.ItemPedidoRepository;
 import com.hector.cursomc.repositories.PagamentoRepository;
 import com.hector.cursomc.repositories.PedidoRepository;
 import com.hector.cursomc.repositories.ProdutoRepository;
@@ -49,6 +50,8 @@ public class CursomcApplication implements CommandLineRunner {
 	private PedidoRepository pedidoRepository;
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -113,6 +116,19 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		pedidoRepository.save(Arrays.asList(ped1,ped2));
 		pagamentoRepository.save(Arrays.asList(pagto1,pagto2));
+		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.save(Arrays.asList(ip1,ip2,ip3));
 		
 	}
 }
--- src/main/java/com/hector/cursomc/domain/Produto.java
+++ src/main/java/com/hector/cursomc/domain/Produto.java
@@ -2,7 +2,9 @@ package com.hector.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,8 +13,10 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+
 
 @Entity
 public class Produto implements Serializable {
@@ -35,6 +39,9 @@ public class Produto implements Serializable {
 	
 	private List<Categoria> categorias = new ArrayList<>();
 	
+	@OneToMany(mappedBy = "id.produto")
+	private Set<ItemPedido> itens = new HashSet<>();
+	
 	public Produto () {		
 	}
 
@@ -43,6 +50,14 @@ public class Produto implements Serializable {
 		this.id = id;
 		this.nome = nome;
 		this.preco = preco;
+	}
+	
+	public List<Pedido> getPedidos(){
+		List<Pedido> lista = new ArrayList<>();
+		for (ItemPedido x: itens) {
+			lista.add(x.getPedido());
+		}
+		return lista;
 	}
 
 	public Integer getId() {
@@ -77,6 +92,14 @@ public class Produto implements Serializable {
 		this.categorias = categorias;
 	}
 
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -101,10 +124,4 @@ public class Produto implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
-	
-	
-	
-	
 }
