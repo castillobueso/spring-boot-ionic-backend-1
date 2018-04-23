79caac0644097d3dc6b8c1769913009258874b2c
Hector Castillo <castillobueso@yahoo.com>
2018-04-23 14:44:50 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/hector/cursomc/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.hector.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hector.cursomc.domain.Pedido;
+import com.hector.cursomc.repositories.PedidoRepository;
+import com.hector.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {		
+		Pedido obj = repo.findOne(id);		
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto no encontrado! Id: " + id
+					+ ", Tipo: " + Pedido.class.getName());
+		}
+		return obj;
+	}
+}
