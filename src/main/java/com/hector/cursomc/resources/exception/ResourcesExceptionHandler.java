2d9b7c03f3f824527fcf5b7d6d44fa873bbcdcab
Hector Castillo <castillobueso@yahoo.com>
2018-04-26 15:00:39 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/com/hector/cursomc/resources/exception/ResourcesExceptionHandler.java
+++ src/main/java/com/hector/cursomc/resources/exception/ResourcesExceptionHandler.java
@@ -6,16 +6,23 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.client.HttpStatusCodeException;
 
+import com.hector.cursomc.services.exceptions.DataIntegrityException;
 import com.hector.cursomc.services.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
 public class ResourcesExceptionHandler {
+	
 	@ExceptionHandler(ObjectNotFoundException.class)
 	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
 		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
 	}
 
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request){
+		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+	
 }
--- src/main/java/com/hector/cursomc/services/CategoriaService.java
+++ src/main/java/com/hector/cursomc/services/CategoriaService.java
@@ -1,10 +1,12 @@
 package com.hector.cursomc.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.hector.cursomc.domain.Categoria;
 import com.hector.cursomc.repositories.CategoriaRepository;
+import com.hector.cursomc.services.exceptions.DataIntegrityException;
 import com.hector.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -30,5 +32,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.delete(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma Categoria que possui produto");
+		}		
 	}
 }
