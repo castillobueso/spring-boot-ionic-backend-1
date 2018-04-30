a1d0d2ebe37ac6ebebd762f4b10b7f9a54eda3d0
Hector Castillo <castillobueso@yahoo.com>
2018-04-30 17:15:26 -0300

    PUT, DELETE e GET para Cliente

---
+++ src/main/java/com/hector/cursomc/dto/ClienteDTO.java
@@ -0,0 +1,59 @@
+package com.hector.cursomc.dto;
+
+import java.io.Serializable;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import com.hector.cursomc.domain.Cliente;
+
+public class ClienteDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	
+	@NotEmpty(message = "Preenchimento Obrigatorio")
+	@Length(min = 5, max = 120, message = "O tamanho deve ser entre 5 y 120 caracteres")
+	private String nome;
+	
+	@NotEmpty(message = "Preenchimento Obrigatorio")
+	@Email(message ="Email Invalido")
+	private String email;
+	
+	// Constructor Vacio
+	public ClienteDTO() {		
+	}
+	
+	// Crear un constructor que recibe una entidade cliente y genera mi Dto
+	public ClienteDTO(Cliente obj) {
+		id 		= obj.getId();
+		nome 	= obj.getNome();
+		email 	= obj.getEmail();
+	}
+
+	// Crear los Getters y Setters
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}	
+}
--- src/main/java/com/hector/cursomc/services/ClienteService.java
+++ src/main/java/com/hector/cursomc/services/ClienteService.java
@@ -1,10 +1,19 @@
 package com.hector.cursomc.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.hector.cursomc.domain.Cliente;
+import com.hector.cursomc.domain.Cliente;
+import com.hector.cursomc.dto.ClienteDTO;
 import com.hector.cursomc.repositories.ClienteRepository;
+import com.hector.cursomc.services.exceptions.DataIntegrityException;
 import com.hector.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -21,4 +30,39 @@ public class ClienteService {
 		}
 		return obj;
 	}
+	
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj,obj);
+		return repo.save(newObj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.delete(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir porque ha entidades relacionadas");
+		}		
+	}
+	
+	public List<Cliente> findAll() {
+		return repo.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+		
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	} 
 }
