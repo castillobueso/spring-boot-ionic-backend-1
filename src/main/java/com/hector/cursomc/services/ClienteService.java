b16ad05a0cc9055d7eb427ef503f19c84c0dd751
Hector Castillo <castillobueso@yahoo.com>
2018-05-10 19:04:45 -0300

    Ajustando e Testando a Deleção de Cliente

--- src/main/java/com/hector/cursomc/services/ClienteService.java
+++ src/main/java/com/hector/cursomc/services/ClienteService.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.hector.cursomc.domain.Cidade;
 import com.hector.cursomc.domain.Cliente;
@@ -42,6 +43,7 @@ public class ClienteService {
 		return obj;
 	}
 	
+	@Transactional
 	public Cliente insert(Cliente obj) {
 		obj.setId(null);
 		obj = repo.save(obj);
@@ -61,7 +63,7 @@ public class ClienteService {
 			repo.delete(id);
 		}
 		catch (DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possível excluir porque ha entidades relacionadas");
+			throw new DataIntegrityException("Não é possível excluir porque ha pedidos relacionadas");
 		}		
 	}
 	
@@ -80,7 +82,9 @@ public class ClienteService {
 	
 	public Cliente fromDTO(ClienteNewDTO objDto) {
 		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.toEnum(objDto.getTipo()));
-		Cidade cid = cidadeRepository.findOne(objDto.getCidadeId());
+		
+		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
+				
 		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid);
 		cli.getEnderecos().add(end);
 		cli.getTelefones().add(objDto.getTelefone1());
